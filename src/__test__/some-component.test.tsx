972dd29558ed1583046fceaf3896c3062ecc258f
Gustav Wengel <gustavwengel@gmail.com>
2017-05-10 18:05:46 +0200

    jest snaps

--- src/__test__/some-component.test.tsx
+++ src/__test__/some-component.test.tsx
@@ -1,5 +1,8 @@
 import 'jest';
+import 'react-native';
+import React from 'react';
 import { SomeComponent } from '../some-component';
+import renderer from 'react-test-renderer';
 
 describe('Basic breakpoints and sourcemaps', () => {
 	it('Should show you the value of the variables the whole way through', () => {
@@ -14,5 +17,11 @@ describe('Some component', () => {
 	it('Should import', () => {
 		const component = SomeComponent;
 		expect(component).toBeDefined();
+	});
+	
+	
+	it('Should render', () => {
+		const renderedComponent = renderer.create(<SomeComponent />).toJSON();
+		expect(renderedComponent).toMatchSnapshot();
 	});
 });
--- src/some-component.tsx
+++ src/some-component.tsx
@@ -16,3 +16,4 @@ export const SomeComponent = () => {
 	</View>
 	)
 };
+
