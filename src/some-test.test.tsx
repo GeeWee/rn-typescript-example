a2b3558648c0d6d8d586d60a4e07b9c58aec7dac
Gustav Wengel <gustavwengel@gmail.com>
2017-05-11 08:44:05 +0200

    commit 2

--- src/some-test.test.tsx
+++ src/some-test.test.tsx
@@ -1,12 +1,23 @@
 import 'jest';
 
+interface SomeInterface {
+	foo: string,
+	bar: number
+}
+
 describe('Basic breakpoints and sourcemaps', () => {
 	it('Should show you the value of the variables the whole way through', () => {
 		const x = 2;
 		let y = 3;
+		y = 3;
+		y = 3;
+		y = 3;
+		y = 3;
+		y = 3;
 		let z = x + y;
 		//Uncomment this to have it stop at the line above.
 		debugger;
+		const foo = 34;
 		expect(z).toEqual(5);
 	});
 });
