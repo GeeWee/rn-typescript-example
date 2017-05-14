91a62bd6b8463f332f8c871b6c9d9cec579d70db
Gustav Wengel <gustavwengel@gmail.com>
2017-05-14 11:49:08 +0200

    reducers

---
+++ src/simpleReducers/index.ts
@@ -0,0 +1,33 @@
+/**
+ * TODO: Describe file contents
+ */
+
+export function createReducerActionPair<StateType>(actionName: string, defaultState: StateType){
+	return {
+		reducer: createStandardReducer<StateType>(actionName, defaultState),
+		action: createStandardAction<StateType>(actionName),
+	}
+
+}
+
+function createStandardAction<StateType>(actionName: string){
+	return (payload: StateType) => ({
+		type: actionName,
+		payload
+	})
+}
+
+function createStandardReducer<StateType>(actionName: string, initialState: StateType){
+	return (state: StateType = initialState, action: StandardAction<StateType>) => {
+		if (action.type === actionName){
+			return action.payload;
+		}
+		return state;
+	}
+}
+
+
+interface StandardAction<StateType> {
+	type: string,
+	payload: StateType
+}
